1f3eca19c3fc55d2f35ad0fad2225f318f5cafdb
Trischler <t21trischlerch>
2022-05-09 12:33:00 +0200

    Month class

--- Haushaltsbuch/Classes/dataklassen.py
+++ Haushaltsbuch/Classes/dataklassen.py
@@ -1,9 +1,11 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import date, datetime
-from dataclasses import field
+from dataclasses import fiel
+from calendar import month_name as months
 
 
 frmt = "%Y-%m-%d"
+
 
 def simple(o):
     return type(o) in [type(str()), type(int()), type(float()), type(bool()), type(None)]
@@ -45,25 +47,40 @@ class Einahme(object):
 
 
 @dataclass
-class Berechnung(object):
-    name: str
-    datum: date = date.today()
-    author: str = "Kein Author"
+class Month(object):
+    id: int 
+    name: str = "Monthly"
     einnahmen: list[Einahme] = field(default_factory=list)
     ausgaben: list[Ausgabe] = field(default_factory=list)
-    filename: str = None
 
     def toDict(self):
         newdict = self.__dict__.copy()
-        newdict["datum"] = self.datum.strftime(frmt)
         newdict["einnahmen"] = [e.toDict() for e in self.einnahmen]
         newdict["ausgaben"] = [e.toDict() for e in self.ausgaben]
         return newdict
 
     def fromDict(d: dict):
-        d["datum"] = datetime.strptime(d["datum"], frmt).date()
         d["einnahmen"] = [Einahme.fromDict(e) for e in d["einnahmen"]]
         d["ausgaben"] = [Ausgabe.fromDict(e) for e in d["ausgaben"]]
+        return Month(**d)
+
+@dataclass
+class Berechnung(object):
+    name: str
+    datum: date = date.today()
+    author: str = "Kein Author"
+    months: list[Month] = field(default_factory=list)
+    filename: str = None
+
+    def toDict(self):
+        newdict = self.__dict__.copy()
+        newdict["datum"] = self.datum.strftime(frmt)
+        newdict["months"] = [m.toDict() for m in self.months]
+        return newdict
+
+    def fromDict(d: dict):
+        d["datum"] = datetime.strptime(d["datum"], frmt).date()
+        d["einnahmen"] = [Month.fromDict(m) for m in d["months"]]
         return Berechnung(**d)
 
     def addEinahme(self, e: Einahme):
@@ -82,3 +99,6 @@ class Berechnung(object):
             geldAusgaben += a.wert
 
         return geldEinnahmen - geldAusgaben
+
+    def createMonths():
+        return [Month(i, months[i]) for i in range(0, 13)]
